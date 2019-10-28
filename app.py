4c6dd1ff370d820770fa30a2659ede224995e74d
jiwen xin <kevinxin@scripps.edu>
2019-10-28 13:59:30 -0700

    update port

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ from handlers.query_handler import *
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/explorer_kgs/api/v1/hint", HintHandler),
-            (r"/explorer_kgs/api/v1/find_edge", SchemaHandler),
-            (r"/explorer_kgs/api/v1/connect", ConnectHandler),
-            (r"/explorer_kgs/api/v1/semantictypes", SemanticTypesHandler),
-            (r"/explorer_kgs/api/v1/predicates", PredicatesHandler),
-            (r"/explorer_kgs/api/v1/associations", AssociationsHandler),
-            (r"/explorer_kgs/api/v1/idtypes", IDTypesHandler),
-            (r"/explorer_kgs/api/v1/filter_edges", EdgeFilterHandler),
-            (r"/explorer_kgs/api/v1/single_hop_query", SingleHopQueryHandler),
-            (r"/explorer_kgs/api/v1/multi_hop_query", MultiHopQueryHandler)
+            (r"/explorer_api/v1/hint", HintHandler),
+            (r"/explorer_api/v1/find_edge", SchemaHandler),
+            (r"/explorer_api/v1/connect", ConnectHandler),
+            (r"/explorer_api/v1/semantictypes", SemanticTypesHandler),
+            (r"/explorer_api/v1/predicates", PredicatesHandler),
+            (r"/explorer_api/v1/associations", AssociationsHandler),
+            (r"/explorer_api/v1/idtypes", IDTypesHandler),
+            (r"/explorer_api/v1/filter_edges", EdgeFilterHandler),
+            (r"/explorer_api/v1/single_hop_query", SingleHopQueryHandler),
+            (r"/explorer_api/v1/multi_hop_query", MultiHopQueryHandler)
         ]
 
         tornado.web.Application.__init__(self, handlers)
@@ -29,7 +29,7 @@ class Application(tornado.web.Application):
 
 def main():
     app = Application()
-    app.listen(8855)
+    app.listen(8856)
     tornado.ioloop.IOLoop.instance().start()
 
 
